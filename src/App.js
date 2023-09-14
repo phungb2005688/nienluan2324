d459d0f94e06345d66b4569e5c28f3d38dcf904c
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2023-09-14 19:24:33 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slices/counterSlice'
-import styled from 'styled-components'
+import React, { Fragment } from 'react'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {routes} from './routes'
+import DefaultComponent from './Components/DefaultComponent/DefaultComponent'
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
-  const Button = styled.button({
-    background: 'green',
-  });
-
   return (
     <div>
-      <div>
-        <Button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return(
+              <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+            }></Route>
+            )
+          })}
+
+        </Routes>
+      </Router>
     </div>
   )
 }
