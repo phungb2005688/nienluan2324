49cf0686e4ae01c9468c1687567969f063cc08f9
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2023-09-15 23:46:00 +0700

    new

---
+++ src/Components/CardComponent/CardComponent.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StyleNameProduct, WrapperCardStyle, WrapperTextDiscount, WrapperTextPrice, WrapperTextReport } from './style'
+import {
+    StarFilled
+  } from '@ant-design/icons';
+import notice from '../../assets/images/notice.png'
+const CardComponent = () => {
+    return (
+        <WrapperCardStyle
+            hoverable
+            headStyle={{width: '200px', height: '200px'}}
+            style={{ width: 200 }}
+            bodyStyle={{ padding: '10px' }}
+            cover={<img alt="example" src="https://cf.shopee.vn/file/58dcbd95089b359d0e9898f5d87d8f81" />}
+        >
+            <img src={notice} 
+                style={{ width: '68px', height: '14px', 
+                        position: 'absolute', top: -1, left: -1,
+                        borderTopLeftRadius: '3px' 
+                    }} alt=''>
+            </img>
+            <StyleNameProduct>Sữa rửa mặt L'Oreal</StyleNameProduct>
+
+            <WrapperTextReport>
+                <span style={{marginRight: '7px'}}>
+                    <span>4.96</span> <StarFilled style={{ fontSize: '12px', gap: '5px', color: '#faad14'}}/>
+                </span>
+                <span>|</span>
+                <span style={{marginLeft: '7px'}}>Đã bán 1000+</span>
+            </WrapperTextReport>
+            <WrapperTextPrice>
+                1.000.000 đ 
+                <WrapperTextDiscount> -5% </WrapperTextDiscount>
+            </WrapperTextPrice>
+
+        </WrapperCardStyle>
+    )
+}
+
+export default CardComponent
--- src/Components/HeaderComponent/HeaderComponent.jsx
+++ src/Components/HeaderComponent/HeaderComponent.jsx
@@ -1,8 +1,49 @@
+import { Col } from 'antd';
 import React from 'react'
+import { WrapperHeader, WrapperHeaderText, WrapperHeaderAccount, WrapperHeaderTextSmall } from './style';
+import {
+  UserOutlined, CaretDownOutlined, ShoppingCartOutlined
+} from '@ant-design/icons';
+import ButtonInputSearch from '../ButtonInputSearch/ButtonInputSearch';
 
 const HeaderComponent = () => {
   return (
-    <div>HeaderComponent</div>
+    <div>
+      <WrapperHeader>
+        <Col span={4}>
+          <WrapperHeaderText>
+            {/* <WrapperIMGHeader style={{ fontSize: '10px'}} ><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/HP_logo_630x630.png/240px-HP_logo_630x630.png" alt="" /></WrapperIMGHeader> */}
+            RINEL
+          </WrapperHeaderText>
+        </Col>
+        <Col span={13}>
+          <ButtonInputSearch
+            size = "large"
+            textButton = "Tìm kiếm"
+            placeholder="Tìm sản phẩm..."
+            bordered={false}
+            //onSearch={onSearch}
+          />
+        </Col>
+        <Col span={7} style={{ display: 'flex', gap: '20px'}}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize:'30px' }}/>
+            <div>
+              <WrapperHeaderTextSmall>Đăng nhập/Đăng ký</WrapperHeaderTextSmall>
+              <div>
+                <WrapperHeaderTextSmall>Tài khoản <CaretDownOutlined /></WrapperHeaderTextSmall>
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div style={{ display: 'flex', alignItems: 'center', color: 'white'}}>
+            <ShoppingCartOutlined style={{ fontSize:'30px', padding: '5px'}}/>
+            <span>Giỏ hàng</span>
+            {/* WrapperHeaderTextSmall */}
+          </div>
+
+        </Col>
+      </WrapperHeader>
+    </div>
   )
 }
 
