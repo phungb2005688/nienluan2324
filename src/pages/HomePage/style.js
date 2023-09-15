4f0b0c1ea44e8d6d183e87c5d631ca89209f7155
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2023-09-15 23:45:15 +0700

    add style

---
+++ src/pages/HomePage/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WrapperProductType = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 27px;
+    justify-contennt: flex-start;
+    // border-bottom: 1px solid;
+    height: 44px;
+    
+`
