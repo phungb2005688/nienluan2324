137722309f555ea6bf6dcfecffd0062314f2b4d0
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2023-11-09 20:31:42 +0700

    connectbackend1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from 'react'
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import {routes} from './routes'
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { routes } from './routes'
 import DefaultComponent from './Components/DefaultComponent/DefaultComponent'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 
 function App() {
+
+  // useEffect(() => {
+  //   fetchApi()
+  // }, [])
+  console.log('process.env.REACT_API_URL_BACKEND', process.env.REACT_APP_API_URL);
+
+  const fetchApi = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
+    return res.data
+  }
+  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+  console.log('query', query);
+
   return (
     <div>
       <Router>
@@ -11,12 +26,12 @@ function App() {
           {routes.map((route) => {
             const Page = route.page
             const Layout = route.isShowHeader ? DefaultComponent : Fragment
-            return(
+            return (
               <Route key={route.path} path={route.path} element={
                 <Layout>
                   <Page />
                 </Layout>
-            }></Route>
+              }></Route>
             )
           })}
 
